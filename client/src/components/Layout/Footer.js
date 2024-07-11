43b69a77732434156415d27a13833185982be1fa
Priyom1 <soikatmirror23@gmail.com>
2024-07-11 11:38:32 +0600

    change check

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='.bg-dark .text-light p-3'>
+      <h4 className='text-center'>
+        Game On!</h4>
+    </div>
+  )
+}
+
+export default Footer
