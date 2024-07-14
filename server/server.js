3abe76ecdc28021ecfc2ef85e10c370a78e2a438
Muhammed Tareq Aziz <muhammedtareqaziz@Muhammeds-MacBook-Pro.local>
2024-07-14 23:10:42 +0600

    cors added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 import express from "express";
-import colors from "colors";
+// import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
